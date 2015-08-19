e7c7cdc323e5bbb4f6b8c630d7670f5a1c23489a
Fabio Fortkamp <fabio@fabiofortkamp.com>
2015-08-19 13:29:29 -0300

    Update settings path in site generation script

--- deploy_tools/siteregen2.sh
+++ deploy_tools/siteregen2.sh
@@ -6,6 +6,6 @@ cd /home/fabiofortkamp/sites/SITENAME
 source activate thermocode
 
 ### Kick that Pelican into action by pointing it at the config file and log the output
-/home/fabiofortkamp/miniconda3/envs/thermocode/bin/pelican -s /home/fabiofortkamp/sites/SITENAME/deploy_tools/publishconf.py >> /home/fabiofortkamp/SITENAME.cron.log 2>&1
+/home/fabiofortkamp/miniconda3/envs/thermocode/bin/pelican -s /home/fabiofortkamp/sites/SITENAME/publishconf.py >> /home/fabiofortkamp/SITENAME.cron.log 2>&1
 ### Timestamp that bad boy.
 echo "siteregen2.sh: $(date)" >> /home/fabiofortkamp/SITENAME.net.cron.log 2>&1
